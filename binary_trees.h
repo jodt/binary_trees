f9ff4a665f113dff022119d69f4cfc7e12b07946
WILLOT Philippe <willot.ph@gmail.com>
2022-03-22 20:55:49 +0000

    adding task 24

--- binary_trees.h
+++ binary_trees.h
@@ -53,6 +53,9 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
+
 /* prototypes utility functions*/
 void currentLevel(const binary_tree_t *tree, void (*func)(int), int level);
 void same_level(int a, int b, const binary_tree_t **first, const binary_tree_t **second);
