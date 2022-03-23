bfa01cccae9f57eb19cba55fe978fb0102b24b70
WILLOT Philippe <willot.ph@gmail.com>
2022-03-23 11:13:02 +0000

    bug fix edge case duplicate task 24

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,67 +1,37 @@
 #include "binary_trees.h"
+#include "limits.h"
 
-int getMax(binary_tree_t *root);
-int getMin(binary_tree_t *root);
+int is_bst_helper(const binary_tree_t *tree, int low, int high);
 
 /**
- * binary_tree_is_bst - checks if a binary tree is a valid BST
- * @tree: pointer to the root node of the tree to check
- * Return: 1 if valid and 0 otherwise
+ * binary_tree_is_bst - Checks if a binary tree is a valid binary search tree.
+ * @tree: A pointer to the root node of the tree to check.
+ *
+ * Return: 1 if tree is a valid BST, and 0 otherwise
  */
-
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (1);
-
-	if (tree->left)
-	{
-		if (getMax(tree->left) > tree->n)
-			return (0);
-	}
-
-	if (tree->right)
-	{
-		if (getMin(tree->right) < tree->n)
-			return (0);
-	}
-
-	return (binary_tree_is_bst(tree->left) && binary_tree_is_bst(tree->right));
+	return (is_bst_helper(tree, INT_MIN, INT_MAX));
 }
 
 /**
- * getMin - Return the minimum value of a BST
- * @root: pointer of the root node
- * Return: min value of the BST
+ * is_bst_helper - Checks if a binary tree is a valid binary search tree.
+ * @tree: A pointer to the root node of the tree to check.
+ * @low: The value of the smallest node visited thus far.
+ * @high: The value of the largest node visited this far.
+ *
+ * Return: If the tree is a valid BST, 1, otherwise, 0.
  */
-
-int getMin(binary_tree_t *root)
+int is_bst_helper(const binary_tree_t *tree, int low, int high)
 {
-	binary_tree_t *temp = root;
-
-	if (root == NULL)
-		return (1);
-
-	while (temp->left)
-		temp = temp->left;
-
-	return (temp->n);
-}
-
-/**
- * getMax - Return the maximum value of a BST
- * @root: pointer of the root node
- * Return: max value of the BST
- */
-int getMax(binary_tree_t *root)
-{
-	binary_tree_t *temp = root;
-
-	if (root == NULL)
-		return (1);
-
-	while (temp->right)
-		temp = temp->right;
-
-	return (temp->n);
+	if (tree != NULL)
+	{
+		if (tree->n < low || tree->n > high)
+			return (0);
+		return (is_bst_helper(tree->left, low, tree->n - 1) &&
+			is_bst_helper(tree->right, tree->n + 1, high));
+	}
+	return (1);
 }
